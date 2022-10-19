66195a0146b0bc80ff3e9e9c3e4391b0ead445e1
Jorge Harbes <jharbes@hotmail.com>
2022-10-19 15:57:37 -0300

    Update constantesComConst.js

--- 02-javascript-basico/constantesComConst.js
+++ 02-javascript-basico/constantesComConst.js
@@ -4,3 +4,10 @@ const segundoNumero = 10;
 const resultado = primeiroNumero * segundoNumero;
 const resultaDuplicado = resultado * 2;
 console.log(resultaDuplicado);
+
+// testando funcao number
+
+let numero='1'
+let numero2='2'
+
+console.log(Number(numero)+Number(numero2))
