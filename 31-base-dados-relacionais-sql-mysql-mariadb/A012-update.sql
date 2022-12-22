2d6dbb45e1410e00156c6bb2c14c698e4e487ec8
Jorge Harbes <jharbes@hotmail.com>
2022-12-22 10:31:20 -0300

    Create A012-update.sql

---
+++ 31-base-dados-relacionais-sql-mysql-mariadb/A012-update.sql
@@ -0,0 +1,9 @@
+-- Update - Atualiza registros
+
+
+update users set 
+first_name = 'Luiz',
+last_name = 'Miranda'
+where id between 119 and 121;
+
+select * from users where id between 119 and 121;
