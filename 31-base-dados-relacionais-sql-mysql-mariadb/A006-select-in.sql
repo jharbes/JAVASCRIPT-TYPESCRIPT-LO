683ec69497c1334436390e0745a1ad297953663a
Jorge Harbes <jharbes@hotmail.com>
2022-12-22 08:29:45 -0300

    Update A006-select-in.sql

--- 31-base-dados-relacionais-sql-mysql-mariadb/A006-select-in.sql
+++ 31-base-dados-relacionais-sql-mysql-mariadb/A006-select-in.sql
@@ -1,4 +1,10 @@
--- in seleciona elementos entre os valores enviados
+-- in seleciona elementos entre todos os valores enviados e retornara os que forem verdadeiros (existirem), 
+-- repare nos comandos abaixo, ambos possuem o mesmo resultado, o in serve para facilitar a escrita da consulta.
+
+
+select * from users
+where id=110 or id=115 or id=120 or id=125 or id= 130 or id=1000 or id=12200 or id=1212545
+and first_name='Luiz' or first_name='Keelie';
 
 
 select * from users
