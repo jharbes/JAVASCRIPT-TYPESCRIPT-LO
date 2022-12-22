c3dfe50599eaf02fb121797a920601d0a9a49ee7
Jorge Harbes <jharbes@hotmail.com>
2022-12-22 10:47:46 -0300

    Update A012-update.sql

--- 31-base-dados-relacionais-sql-mysql-mariadb/A012-update.sql
+++ 31-base-dados-relacionais-sql-mysql-mariadb/A012-update.sql
@@ -4,6 +4,6 @@
 update users set 
 first_name = 'Luiz',
 last_name = 'Miranda'
-where id between 119 and 121;
+where id between 119 and 121; -- nesse caso todos os registros onde o id for de 119 ate 121 terao seu primeiro nome mudado para Luiz e ultimo nome alterado para 'Miranda'
 
 select * from users where id between 119 and 121;
