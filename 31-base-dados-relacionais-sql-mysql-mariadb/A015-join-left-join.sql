7a2f59685ead1b331830118c1148063065c4593d
Jorge Harbes <jharbes@hotmail.com>
2022-12-22 12:15:58 -0300

    Update A015-join-left-join.sql

--- 31-base-dados-relacionais-sql-mysql-mariadb/A015-join-left-join.sql
+++ 31-base-dados-relacionais-sql-mysql-mariadb/A015-join-left-join.sql
@@ -1,18 +1,13 @@
--- Seleciona users.id, profiles.id, profiles.bio
--- profiles.description, users.first_name
--- da tabela users (todos os registros da tabela da esquerda)
--- unindo com a tabela profiles (tabela da direita é opcional)
--- quando a condição u.id = p.user_id for satisfeita
--- onde users.first_name terminar com "a"
--- ordena por users.first_name decrescente
--- limita 5 registros
+-- Nesse caso ele retornara todos os registros da tabela users (left)
+-- mesmo que eles nao tenham nenhuma ligacao com a tabela profiles (right)
+-- nesses ultimos casos ele retornara o vinculo como nulo
+
+-- IMPORTANTE***: ele ainda retorna a consulta de profiles, mas a preferencia eh apenas pela tabela users,
+-- ou seja, ele so retornara de profiles aqueles que tiverem ligacao com users, de users retornara tudo.
 
 
 SELECT u.id as uid, p.id as pid,
 p.bio, u.first_name
 FROM users as u
 LEFT JOIN profiles p
-ON u.id = p.user_id
-WHERE u.first_name LIKE '%a'
-ORDER BY u.first_name DESC
-LIMIT 5;
+ON u.id = p.user_id;
