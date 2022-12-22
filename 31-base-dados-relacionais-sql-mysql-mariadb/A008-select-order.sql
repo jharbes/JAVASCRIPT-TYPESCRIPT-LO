2bc9a275b10b07d33a563f72e4b358cddb7de89c
Jorge Harbes <jharbes@hotmail.com>
2022-12-22 09:28:38 -0300

    Update A008-select-order.sql

--- 31-base-dados-relacionais-sql-mysql-mariadb/A008-select-order.sql
+++ 31-base-dados-relacionais-sql-mysql-mariadb/A008-select-order.sql
@@ -8,3 +8,9 @@ select id, first_name, email as uemail
 from users
 where id between 100 and 150
 order by first_name desc;
+
+
+select id, first_name, email as uemail 
+from users
+where id between 100 and 150
+order by first_name desc, last_name asc; -- nesse caso ele da prioridade para a primeira consulta (first name), levando em consideracao que hajam nomes iguais esses entao serao ordenados pelo ultimo nome de forma crescente.
